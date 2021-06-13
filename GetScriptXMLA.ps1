f7b96615f02195fbbbb15cc95dbfb81454fcef85
ouakniny <52452983+ouakniny@users.noreply.github.com>
2021-06-13 09:21:58 +0300

    Add files via upload

---
+++ GetScriptXMLA.ps1
@@ -0,0 +1,12 @@
+﻿$rootfolder = 'E:\backup\Flex\'
+$Filepath       = $rootfolder+'Mahout_Backup_'+$date # local directory to save build-scripts to
+
+$ssas_deployment = 'Microsoft.AnalysisServices.Deployment.exe'
+$asdatabase = 'E:\sources\Ilan\BI Developers\FlexTabular\Master\FlexTabular\bin\Model.asdatabase'
+$pathxmla = $Filepath+'\FlexTabular.xmla'
+$arglist = @(
+	$asdatabase,	
+	"/d",
+	"/o:$pathxmla"
+)
+Start-Process -FilePath $ssas_deployment -ArgumentList $arglist
