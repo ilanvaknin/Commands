c0efbe60ddd36b057600498f6a6d1b631e900dd8
ouakniny <52452983+ouakniny@users.noreply.github.com>
2022-03-08 12:28:09 +0200

    Add files via upload

---
+++ query_msg_files.ps1
@@ -0,0 +1,10 @@
+﻿$outlook = New-Object -comobject outlook.application
+
+Get-ChildItem "C:\data\mail\kidum" -Filter  *.msg |
+ForEach-Object{
+  $msg = $outlook.CreateItemFromTemplate($_.FullName)
+  $msg | Select to,subject| Export-csv -Append "C:\data\mail\kidum\output.csv" -Encoding UTF8
+  $null = [System.Runtime.Interopservices.Marshal]::ReleaseComObject($msg);
+}
+$null = [System.Runtime.Interopservices.Marshal]::ReleaseComObject($outlook);
+[System.GC]::Collect(); [System.GC]::WaitForPendingFinalizers();
