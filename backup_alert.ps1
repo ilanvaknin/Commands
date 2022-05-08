a9920eb4e02c679bd025315960c098d224c8d121
ouakniny <52452983+ouakniny@users.noreply.github.com>
2022-05-08 14:48:07 +0300

    Add files via upload

---
+++ backup_alert.ps1
@@ -0,0 +1,29 @@
+﻿$Output = "C:\Folder\To\Check\*"
+
+$Number_of_items = (Get-ChildItem -Path $Output -Recurse | Measure-Object).Count
+
+$hostname = hostname
+
+If ($Number_of_items -gt 4)
+{
+    $MailArgs = @{
+            'To'          = "ylanou@gmail.com"
+            'From'        = "adminuser@domain.co.il"
+            'Subject'     = "$hostname Failure - Too much mahout backup files"
+            'Body'        = "$hostname Failure - Number of items on G:\App_Backups\Flex\backup\ IS $Number_of_items "
+            'SmtpServer' = ""
+            }
+}
+
+else {
+$MailArgs = @{
+        'To'          = "ylanou@gmail.com"
+        'From'        = "adminuser@domain.co.il"
+        'Subject'     = "$hostname Success - The amount of transaction logs reasonable"
+        'Body'        = "$hostname Success - Number of items on D:\ExchangeData IS $Number_of_items "
+        'SmtpServer' = ""
+          
+            }      
+}
+
+Send-MailMessage @MailArgs
---
+++ move_archive.ps1
@@ -0,0 +1,11 @@
+﻿$Source = "\\folder\from\*.zip"
+$Dest   = "\\folder\to"
+$Username = "user"
+$Password = ConvertTo-SecureString "xxxxxxxxxxxxx" -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential($Username, $Password)
+
+New-PSDrive -Name J -PSProvider FileSystem -Root $Dest -Credential $mycreds -Persist
+
+Move-Item -Path $Source -Destination "J:\"
+
+Remove-PSDrive -Name J
